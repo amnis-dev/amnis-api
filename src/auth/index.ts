2aa0d58555bb556972e202c588ba756ffe063bde
Eric Crowell <eric.crowell@ecrow.dev>
2023-02-23 19:03:32 +0100

    feat(Processers): Processers structure updated to match new amnis state type

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,3 +1,4 @@
+import type { IoProcesses } from '@amnis/state';
 import { processAuthChallenge } from './auth.challenge.js';
 import { processAuthCreate } from './auth.create.js';
 import { processAuthCredential } from './auth.credential.js';
@@ -7,15 +8,17 @@ import { processAuthOtp } from './auth.otp.js';
 import { processAuthRegister } from './auth.register.js';
 import { processAuthVerify } from './auth.verify.js';
 
-export const processAuth = {
-  challenge: processAuthChallenge,
-  create: processAuthCreate,
-  credential: processAuthCredential,
-  otp: processAuthOtp,
-  register: processAuthRegister,
-  login: processAuthLogin,
-  logout: processAuthLogout,
-  verify: processAuthVerify,
+export const processAuth: IoProcesses = {
+  post: {
+    challenge: processAuthChallenge,
+    create: processAuthCreate,
+    credential: processAuthCredential,
+    otp: processAuthOtp,
+    register: processAuthRegister,
+    login: processAuthLogin,
+    logout: processAuthLogout,
+    verify: processAuthVerify,
+  },
 };
 
 export default processAuth;
--- src/crud/index.ts
+++ src/crud/index.ts
@@ -1,13 +1,16 @@
+import type { IoProcesses } from '@amnis/state';
 import { processCrudCreate } from './crud.create.js';
 import { processCrudRead } from './crud.read.js';
 import { processCrudUpdate } from './crud.update.js';
 import { processCrudDelete } from './crud.delete.js';
 
-export const processCrud = {
-  create: processCrudCreate,
-  read: processCrudRead,
-  update: processCrudUpdate,
-  delete: processCrudDelete,
+export const processCrud: IoProcesses = {
+  post: {
+    create: processCrudCreate,
+    read: processCrudRead,
+    update: processCrudUpdate,
+    delete: processCrudDelete,
+  },
 };
 
 export default processCrud;
