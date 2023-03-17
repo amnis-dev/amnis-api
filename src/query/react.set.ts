0ce60e8ab146aed28822c7654e1ccb7b12a78485
Eric Crowell <eric.crowell@ecrow.dev>
2023-03-17 17:57:12 +0100

    feat(System): Added react version of system endpoints

--- src/query/react.set.ts
+++ src/query/react.set.ts
@@ -1,4 +1,6 @@
 import type { Set } from '@amnis/state';
+import { apiSys } from './sys/index.react.js';
+import { apiSysMiddleware } from './sys/sys.mw.js';
 import { apiAuth } from './auth/index.react.js';
 import { apiAuthMiddleware } from './auth/auth.mw.js';
 import { apiCrud } from './crud/index.react.js';
@@ -6,6 +8,7 @@ import { apiCrudMiddleware } from './crud/crud.mw.js';
 import { apiMiddleware } from './mw.js';
 
 const reducerApi = {
+  [apiSys.reducerPath]: apiSys.reducer,
   [apiAuth.reducerPath]: apiAuth.reducer,
   [apiCrud.reducerPath]: apiCrud.reducer,
 };
@@ -13,9 +16,11 @@ const reducerApi = {
 export const setApi: Set<typeof reducerApi> = {
   reducers: reducerApi,
   middleware: [
+    apiSys.middleware,
     apiAuth.middleware,
     apiCrud.middleware,
     apiMiddleware,
+    apiSysMiddleware,
     apiAuthMiddleware,
     apiCrudMiddleware,
   ],
