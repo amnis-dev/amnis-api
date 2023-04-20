fb9874db5b0d1a643c1f82b69e26b663c9aaddb0
Eric Crowell <eric.crowell@ecrow.dev>
2023-04-20 10:26:57 +0200

    feat(Sys): System now ensures api URLs are absolute

--- src/query/sys/sys.mw.ts
+++ src/query/sys/sys.mw.ts
@@ -1,5 +1,6 @@
+import type { Api, Entity } from '@amnis/state';
 import {
-  dataActions, systemSlice, systemKey,
+  dataActions, systemSlice, systemKey, apiSlice,
 } from '@amnis/state';
 import type { Middleware } from '@reduxjs/toolkit';
 import { apiSys } from './sys.api.js';
@@ -15,7 +16,32 @@ export const apiSysMiddleware: Middleware = () => (next) => (action) => {
       return next(action);
     }
 
-    next(dataActions.insert(result));
+    /**
+     * Remap the API base URLs to be absolute if they aren't.
+     */
+    const url = new URL(originalArgs.url);
+    const { origin } = url;
+    const apis = (result[apiSlice.key] ?? []) as Entity<Api>[];
+    const apisRemapped = apis.map<Entity<Api>>((api) => {
+      const baseUrl = api.baseUrl ?? '';
+      const apiUrlAbsolute = /^(?:[a-z+]+:)?\/\//.test(baseUrl);
+
+      if (apiUrlAbsolute) {
+        return api;
+      }
+
+      const newBaseUrl = new URL(baseUrl, origin ?? 'http://localhost').href;
+
+      return {
+        ...api,
+        baseUrl: newBaseUrl,
+      };
+    });
+
+    next(dataActions.insert({
+      ...result,
+      [apiSlice.key]: apisRemapped,
+    }));
 
     if (originalArgs?.set) {
       const system = result[systemKey]?.[0];
